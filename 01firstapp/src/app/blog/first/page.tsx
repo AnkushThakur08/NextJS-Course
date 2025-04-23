08f5ba073191a2874f63eefd8bc1083ef3ae1b30
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2025-04-23 10:37:04 +0000

    first Commit

---
+++ 01firstapp/src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+const BlogFirst = () => {
+    return (
+        <div>
+            <h1>Blog First</h1>
+        </div>
+    )
+}
+
+export default BlogFirst
---
+++ 01firstapp/src/app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+const BlogSecond = () => {
+    return (
+        <div>
+            <h1>Blog Second</h1>
+        </div>
+    )
+}
+
+export default BlogSecond
