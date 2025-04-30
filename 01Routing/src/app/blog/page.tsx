33b338f859ec29e334996ad23cf409c576841208
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2025-04-30 11:35:05 +0000

    loading

--- 01Routing/src/app/blog/page.tsx
+++ 01Routing/src/app/blog/page.tsx
@@ -1,4 +1,9 @@
-const Blog = () => {
+const Blog = async () => {
+  await new Promise(() => {
+    setTimeout((resolve) => {
+      resolve("With a delay");
+    }, 2000);
+  });
   return (
     <div>
       <h1>Blog Page</h1>
